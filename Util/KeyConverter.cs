506b8b658f8afa9454fb75866ad92b1a3e44010c
ssbaek <ssbaek@finaldata.com>
2024-07-29 18:16:05 +0900

    설정 key converter

---
+++ Util/KeyConverter.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Input;
+
+namespace D4Macro.Util;
+
+public class KeyConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is Key key)
+        {
+            return key.ToString();
+        }
+
+        return string.Empty;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is System.Windows.Visibility visibility)
+        {
+            return visibility == System.Windows.Visibility.Visible;
+        }
+        return false;
+    }
+}
